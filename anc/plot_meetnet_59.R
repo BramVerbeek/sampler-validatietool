d05ccaa8bde7049fedfed0056c237869f0574ca6
BramVerbeek <verbeekbram@icloud.com>
2024-09-23 11:28:49 +0200

    adding plot section for net 59

--- anc/plot_meetnet_59.R
+++ anc/plot_meetnet_59.R
@@ -1,9 +1,133 @@
-# Plotting/analysis function for "meetnet-50"
-plot_meetnet_59 <- function(df) {
-  # Example: Create a basic scatter plot
-  plot <- ggplot(df, aes(x = some_column, y = another_column)) +
-    geom_point() +
-    theme_minimal()
-  
-  return(plot)
+plot_meetnet_59 <- function(df, meetpost) {
+
+pdf <- process_meetnet_59(df)[[meetpost]]
+
+parameter_lijst <- colnames(pdf)
+parameter_lijst <- setdiff(parameter_lijst, c("Monsternummer", "Begindatum", "Einddatum", "Validatiecode", "Commentaar", "CommentaarAnt"))
+
+longdf <- pdf %>%
+      pivot_longer(cols = parameter_lijst, names_to = "Parameter", values_to = "Resultaat") %>%
+      filter(!is.na(Resultaat)) %>%
+      mutate(Resultaat = as.numeric(Resultaat), date = as.Date(Einddatum, format = "%d-%m-%Y"))
+
+df2 <- df %>%
+    mutate(date = as.Date(Einddatum, format = "%d-%m-%Y")) %>%
+    arrange(date)
+
+colors <- viridis(24)
+
+TSPlot <- plot_ly(longdf, 
+                x = ~date,
+                y = ~Resultaat, 
+                type = 'scatter',
+                mode = 'lines+markers',
+                color = ~Parameter,   
+                colors = colors,                      
+                marker = list(size = 5),
+                hoverinfo = "x+y+text",              
+                text = ~paste("Parameter:", Parameter)) %>%
+        layout(title = list(text = "<b>Tijdreeks per parameter voor de gekozen meetpost</b>",
+                            x = 0,                                     
+                            xanchor = "left",                            
+                            font = list(                         
+                                size = 16                      
+                            )
+        ),
+            xaxis = list(title = "Einddatum", type = "date", autorange = TRUE),  
+            yaxis = list(title = "Concentratie (µg/m³)", autorange = TRUE),
+            margin = list(l = 40, r = 40, t = 40, b = 40),
+            showlegend = FALSE)
+
+
+PBoxplot <- plot_ly(longdf, 
+                x = ~Parameter, 
+                y = ~Resultaat, 
+                type = 'box',
+                color = ~Parameter,                
+                colors = colors,                  
+                marker = list(size = 5),
+                boxpoints = "all",
+                jitter = 0.5, 
+                pointpos = 0,
+                hoverinfo = "all") %>%
+        layout(title = list(text = "<b>Boxplot per parameter voor de gekozen meetpost</b>",
+                            x = 0,                                     
+                            xanchor = "left",                            
+                            font = list(                         
+                                size = 16                      
+                            )
+        ),
+            xaxis = list(title = "Parameter", autorange = TRUE),
+            yaxis = list(title = "Concentratie (µg/m³)", autorange = TRUE),
+            margin = list(l = 40, r = 40, t = 40, b = 40),
+            showlegend = FALSE)
+
+M <- pdf %>%
+    select(all_of(parameter_lijst)) %>%
+    mutate(across(everything(), as.numeric)) %>%
+    cor(use = "complete.obs")
+
+greys_palette <- colorRampPalette(c("black", "grey"))(n = length(unique(df$MeetpostOpstelling)))
+
+CORplot <- corrplot(M, type = 'lower', order = 'hclust', tl.col = 'black',
+             cl.ratio = 0.2, tl.srt = 45, title = "Correlatie tussen de parameters voor de gekozen meetpost",
+             mar = c(0,0,2,0))
+
+TSPlot2 <- plot_ly(df2 %>% filter(MeetpostOpstelling != meetpost), 
+                    x = ~date,
+                    y = ~Resultaat,
+                    color = ~MeetpostOpstelling,
+                    colors = greys_palette, 
+                    type = 'scatter',
+                    mode = 'lines+markers',
+                    frame = ~Parameter,                      
+                    marker = list(size = 5),
+                    hoverinfo = "x+y+text",              
+                    text = ~paste("Meetpost:", MeetpostOpstelling)) %>% 
+                    layout(title = list(text = "<b>Tijdreeks van de metingen per parameter</b>",
+                                        x = 0,                                     
+                                        xanchor = "left",                            
+                                        font = list(                         
+                                            size = 16                      
+                                        )
+                    ),
+                        xaxis = list(title = "Einddatum", type = "date", autorange = TRUE),  
+                        yaxis = list(title = "Concentratie (µg/m³)", autorange = TRUE),
+                        margin = list(l = 40, r = 40, t = 40, b = 40),
+                        showlegend = FALSE) %>%  
+                    add_trace(data = df2 %>% filter(MeetpostOpstelling == meetpost),
+                              marker = list(color = '#ff0000'),
+                              line = list(color = '#ff0000'))
+
+
+BBoxplot <- plot_ly(df %>% filter(MeetpostOpstelling != meetpost), 
+                            x = ~MeetpostOpstelling, 
+                            y = ~Resultaat, 
+                            type = 'box',
+                            color = ~MeetpostOpstelling,
+                            colors = greys_palette,
+                            frame = ~Parameter,                 
+                            marker = list(size = 5),
+                            boxpoints = "all",
+                            jitter = 0.5, 
+                            pointpos = 0,
+                            hoverinfo = "all") %>%
+                    layout(title = list(text = "<b>Boxplot van de metingen per parameter</b>",
+                                        x = 0,                                     
+                                        xanchor = "left",                            
+                                        font = list(                         
+                                            size = 16                      
+                                        )
+                    ),
+                        xaxis = list(title = "Meetpost", autorange = TRUE),
+                        yaxis = list(title = "Concentratie (µg/m³)", autorange = TRUE),
+                        margin = list(l = 40, r = 40, t = 40, b = 40),
+                        showlegend = FALSE) %>%  
+                    add_trace(data = df %>% filter(MeetpostOpstelling == meetpost),
+                              marker = list(color = '#ff0000'),
+                              line = list(color = '#ff0000'),
+                              fillcolor = '#ff9595')
+
+
+return(list(TSPlot, PBoxplot, CORplot, TSPlot2, BBoxplot))
 }
--- app.R
+++ app.R
@@ -1,34 +1,90 @@
-pacman::p_load(shiny, readxl, dplyr, tidyr, ggplot2, lubridate, miceadds)
+pacman::p_load(shiny, readxl, dplyr, tidyr, ggplot2, lubridate, miceadds,
+               DT, plotly, viridis, corrplot)
 source.all("anc/")
-addResourcePath("sty", "sty")
+addResourcePath("www", "www")
+
+
+callback <- c(
+  "var id = $(table.table().node()).closest('.datatables').attr('id');",
+  "$.contextMenu({",
+  "  selector: '#' + id + ' td.factor input[type=text]',",
+  "  trigger: 'hover',",
+  "  build: function($trigger, e){",
+  "    var levels = $trigger.parent().data('levels');",
+  "    if(levels === undefined){",
+  "      var colindex = table.cell($trigger.parent()[0]).index().column;",
+  "      levels = table.column(colindex).data().unique();",
+  "    }",
+  "    var options = levels.reduce(function(result, item, index, array){",
+  "      result[index] = item;",
+  "      return result;",
+  "    }, {});",
+  "    return {",
+  "      autoHide: true,",
+  "      items: {",
+  "        dropdown: {",
+  "          name: 'Edit',",
+  "          type: 'select',",
+  "          options: options,",
+  "          selected: 0",
+  "        }",
+  "      },",
+  "      events: {",
+  "        show: function(opts){",
+  "          opts.$trigger.off('blur');",
+  "        },",
+  "        hide: function(opts){",
+  "          var $this = this;",
+  "          var data = $.contextMenu.getInputValues(opts, $this.data());",
+  "          var $input = opts.$trigger;",
+  "          $input.val(options[data.dropdown]);",
+  "          $input.trigger('change');",
+  "        }",
+  "      }",
+  "    };",
+  "  }",
+  "});"
+)
+
+createdCell <- function(levels){
+  if(missing(levels)){
+    return("function(td, cellData, rowData, rowIndex, colIndex){}")
+  }
+  quotedLevels <- toString(sprintf("\"%s\"", levels))
+  c(
+    "function(td, cellData, rowData, rowIndex, colIndex){",
+    sprintf("  $(td).attr('data-levels', '[%s]');", quotedLevels),
+    "}"
+  )
+}
 
 ui <- navbarPage(
   title = div(
     class = "navbar-brand",
-    tags$img(src = "sty/logo.png", height = "50px"),  
-    span("Validatietool Samplers")
+    tags$img(src = "www/logo.png", height = "50px"),  
+    span("Validatietool Samplermetingen")
   ),
   header = tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "sty/styles.css")
+    tags$link(rel = "stylesheet", type = "text/css", href = "www/styles.css")
   ),
+
 
   tabPanel("Data Invoer", 
            fluidPage(
-
-             titlePanel("Importeer uw ruwe datafile"),
-
-             sidebarLayout(
-               sidebarPanel(
-                selectInput("meetnet", "Selecteer Meetnet:",
-                             choices = c("59")),
-                fileInput("file", "Upload Excel File", 
-                           accept = c(".xls", ".xlsx")),
-                uiOutput("meetpostopstelling_ui")
-               ),
-
-               mainPanel(
-                 tableOutput("data_summary"),
-                 tags$style(HTML("
+#            h4("Inladen ruwe data"),
+            fluidRow(
+              column(6, 
+                     selectInput("meetnet", "Selecteer Meetnet:",
+                                 choices = c("59"))
+              ),
+              column(6, 
+                     fileInput("file", "Upload Excel File", 
+                               accept = c(".xls", ".xlsx"))
+              )
+            ),
+            uiOutput("meetpostopstelling_ui"),
+            dataTableOutput("data_summary"),
+              tags$style(HTML("
                    table {
                    width: 100%;
                    table-layout: auto;
@@ -36,20 +92,49 @@ ui <- navbarPage(
                    th, td {
                    white-space: nowrap;
                    }
-                 "))
-               )
-             )
+              "))
            )
   ),
+  tabPanel("Plots", 
+    fluidPage(
+      h4(textOutput("plot_title")),
+      plotlyOutput("plot1"),
+      plotlyOutput("plot2"),
+      plotOutput("plot3", height = "600px"),
+      plotlyOutput("plot4"),
+      plotlyOutput("plot5")
+    )
+  ),
   tabPanel("Validatie", 
-fluidPage(
-  h4(textOutput("validation_title"))
-          )
+    fluidPage(
+      h4(textOutput("validation_title")),
+      DTOutput("dtable")
+    )
   ),
   tabPanel("Data Export", 
-         fluidPage(
-         )
-  )
+    fluidPage(
+    )
+  ),
+  tabPanel("Info",
+    fluidPage(
+      tags$div(
+        style = "text-align: justify; font-size: 16px;",
+      HTML("<p>
+        Deze <b><a href='https://shiny.posit.co/'>R Shiny app</a></b> werd ontwikkeld bij de dienst Lucht van de Vlaamse Milieumaatschappij 
+        om het valideren van de data komende van verscheidene sampler-meetnetten te vergemakkelijken. <br>
+        Na het importeren van de data wordt de validatie per meetpost uitgevoerd. Hierna kan de data
+        geëxporteerd worden naar een Excel-bestand met een vast bepaalde formatting. <br>
+        <b>Opgelet:</b>  het opnieuw laden van de pagina zal al je voortgang wissen. Zorg ervoor dat je de data hebt geëxporteerd voordat je de pagina herlaadt. <br><br>
+
+        De applicatie is in continue aanbouw, zowel qua functionaliteiten als het toevoegen van nieuwe meetnetten.
+        Voor de nieuwste versie, zie de 
+        <b><a href='https://github.com/BramVerbeek/sampler-validatietool'>GitHub repository</a></b>. <br>
+        Deze tool kwam tot stand met behulp van generatieve AI. Bij vragen, mail naar 
+        <b><a href='mailto:b.verbeek@vmm.be'>b.verbeek@vmm.be</a></b>.
+      </p>")
+        )
+      )
+    )
 )
 
 
@@ -68,18 +153,64 @@ server <- function(input, output, session) {
   })
 
   output$meetpostopstelling_ui <- renderUI({
-    req(processed_data())
+    tagList(
+      tags$div(
+        style = "text-align: justify; font-size: 16px;",
+        HTML("
+          <p>
+        Hier <b>selecteert u de meetpost</b> waarmee u in de rest van het script aan de slag gaat. 
+        De validatie <b>gaat verder op het volgende tabblad</b>. <br> <b>Om de meetpost aan te passen, keert u terug naar dit tabblad.</b> <br>
+        Ten slotte kan u de <b>volledige gevalideerde dataset exporteren</b> naar een Excel-bestand op het Data Export tabblad.
+          </p>
+        ")
+      ),
     selectInput("meetpostopstelling", "Selecteer Meetpost:",
                 choices = names(processed_data()))
+    )
   })
 
-  output$data_summary <- renderTable({
+  output$data_summary <- renderDataTable({
     req(input$meetpostopstelling)
-    processed_data()[[input$meetpostopstelling]] 
+    datatable(processed_data()[[input$meetpostopstelling]])
+  })
+
+  output$plot1 <- renderPlotly({
+    plot1 <- get(paste0("plot_meetnet_", input$meetnet))(uploaded_data(),input$meetpostopstelling)
+    plot1[[1]]
+  })
+
+  output$plot2 <- renderPlotly({
+    plot2 <- get(paste0("plot_meetnet_", input$meetnet))(uploaded_data(),input$meetpostopstelling)
+    plot2[[2]] 
+  })
+
+  output$plot3 <- renderPlot({
+    plot3 <- get(paste0("plot_meetnet_", input$meetnet))(uploaded_data(),input$meetpostopstelling)
+    plot3[[3]] 
+  })
+
+  output$plot4 <- renderPlotly({
+    plot4 <- get(paste0("plot_meetnet_", input$meetnet))(uploaded_data(),input$meetpostopstelling)
+    plot4[[4]] 
+  })
+
+  output$plot5 <- renderPlotly({
+    plot5 <- get(paste0("plot_meetnet_", input$meetnet))(uploaded_data(),input$meetpostopstelling)
+    plot5[[5]] 
   })
 
   output$validation_title <- renderText({
+    req(input$meetpostopstelling)
     paste("Validatie voor Meetnet ", input$meetnet, " en Meetpostopstelling ", input$meetpostopstelling, ":", sep = "")
+  })
+  output$plot_title <- renderText({
+    req(input$meetpostopstelling)
+    paste("Plots voor Meetnet ", input$meetnet, " en Meetpostopstelling ", input$meetpostopstelling, ":", sep = "")
+  })
+
+  output$dtable <- renderDataTable({
+    req(input$meetpostopstelling)
+    datatable(processed_data()[[input$meetpostopstelling]])
   })
 
 }
